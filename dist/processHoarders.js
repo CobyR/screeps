92a4679808b7874fb0851e006c79b4cf657dd42c
Coby Randquist <randquistcp@gmail.com>
2015-10-05 08:20:53 -0700

    fixed issue with duplicate processHoarder code being in two files allowing the wrong one to be edited

--- dist/processHoarders.js
+++ dist/processHoarders.js
@@ -1,11 +1,19 @@
 module.exports = function(hoarders, p_room) {
   var hoard = require('hoarder');
+  var hoardRCL = require('hoarderRcl');
 
   if(hoarders.length > 0){
     console.log('[Hoarders] --------------');
+    var i = 0;
     for(var id in hoarders) {
       var creep = Game.getObjectById(hoarders[id]);
-      hoard(creep, p_room);
+      i++;
+      console.log( i + " " + i % 2);
+      if(i % 2 == 0) {
+        hoardRCL(creep);
+      } else {
+        hoard(creep, p_room);
+      }
     }
   }
 };
