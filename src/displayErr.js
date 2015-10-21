b0943828167e7f167de92409ca6c7b0111bbfd93
Coby Randquist <randquistcp@gmail.com>
2015-10-21 07:38:11 -0700

    added ERR_NAME_EXISTS to display Error

--- src/displayErr.js
+++ src/displayErr.js
@@ -22,6 +22,8 @@ function displayErr(results) {
     return 'ERR_NOT_OWNER';
   case -2:
     return 'ERR_NO_PATH';
+  case -3:
+    return 'ERR_NAME_EXISTS';
   case -4:
     return 'ERR_BUSY';
   case -6:
