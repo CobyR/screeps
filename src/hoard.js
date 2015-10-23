953d81bcf90ced7192499b23f0a878a19fcfca79
Coby Randquist <randquistcp@gmail.com>
2015-10-22 18:59:15 -0700

    work to make hoarders function in more than one room

--- src/hoard.js
+++ src/hoard.js
@@ -2,33 +2,40 @@ function hoard(creep, source_index) {
 
   var busy = 0;
 
-  if(creep.spawning === true) {
-    lca(creep, 'is still spawning.');
-    return 0;
-  }
+  switch(creep.memory.state){
+  case 'transferring':
+    var target = creep.room.storage;
 
-  if(creep.carry.energy === 0 || creep.memory.state == 'gathering') {
-    // if(creep.room.name == 'W11S25') {
-    //  var results = moveToDest(creep,'W11S26');
-    // } else {
-      var sources = creep.room.find(FIND_SOURCES);
-      lca(creep, 'is gathering energy ' + creep.carry.energy + ' of ' + creep.carryCapacity + '.');
-      creep.moveTo(sources[source_index]);
-      creep.harvest(sources[source_index]);
-      if(creep.carry.energy == creep.carryCapacity) {
-        creep.memory.state = 'transferring';
-      } else {
-        creep.memory.state = 'gathering';
-      }
-    // }
-  } else {
-    if(creep.room.name == p_room.name){
-      var target = creep.room.storage;
-      lca(creep, 'is taking energy (' + creep.carry.energy + ') to storage (' + nwc(target.store.energy) + ' of ' + nwc(target.storeCapacity) + ').');
+    if(target){
+      lca(creep, 'is taking energy (' +
+          creep.carry.energy +
+          ') to storage (' +
+          nwc(target.store.energy) +
+          ' of ' + nwc(target.storeCapacity) + ').');
+
       creep.moveTo(creep.room.storage);
       creep.transferEnergy(creep.room.storage);
-    } else {
-      moveToDestinationRoom(creep, p_room.name);
     }
+    break;
+  case 'gathering':
+    var sources = creep.room.find(FIND_SOURCES);
+
+    lca(creep, 'is gathering energy ' + creep.carry.energy + ' of ' + creep.carryCapacity + '.');
+
+    console.log('source_index: ' + source_index + ' sources.length: ' + sources.length);
+
+    if(source_index >= sources.length){
+      source_index = sources.length -1;
+    }
+
+    creep.moveTo(sources[source_index]);
+    creep.harvest(sources[source_index]);
+
+    if(creep.carry.energy == creep.carryCapacity) {
+      creep.memory.state = 'transferring';
+    }
+    break;
+  default:
+    creep.memory.state='gathering';
   }
 }
--- src/processHoarders.js
+++ src/processHoarders.js
@@ -6,12 +6,18 @@ function processHoarders(hoarders) {
     var i = 0;
     for(var id in hoarders) {
       var creep = Game.getObjectById(hoarders[id]);
-      i++;
-      // console.log( i + " " + i % 2);
-      if(creep.room.name == p_room.name && i % 2 === 0 && creep.room.storage.store.energy > 100000 + creep.carryCapacity && HOARD_REMOTE === true) {
-         hoardRCL(creep);
-       } else {
-         hoard(creep, i % 2);
+
+      if(creep.spawning === true) {
+        lca(creep, 'is still spawning.');
+        return 0;
+      } else {
+        i++;
+        // console.log( i + " " + i % 2);
+        if(creep.room.name == p_room.name && i % 2 === 0 && creep.room.storage.store.energy > 100000 + creep.carryCapacity && HOARD_REMOTE === true) {
+          hoardRCL(creep);
+        } else {
+          hoard(creep, i % 2);
+        }
       }
     }
   }
