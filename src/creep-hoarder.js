82c3773f57bb4b9bdc687ac0d4b8a65f9a9c19f6
Coby Randquist <randquistcp@gmail.com>
2015-10-29 11:27:35 -0700

    more refactoring

--- src/creep-hoarder.js
+++ src/creep-hoarder.js
@@ -100,3 +100,22 @@ function hoard(creep, source_index) {
     creep.memory.state = 'gathering';
   }
 }
+
+function remoteHoard(creep) {
+
+  if(creep.spawning) {
+    lca(creep, 'is still spawning.');
+    return 0;
+  }
+
+
+  if(creep.memory.state == 'fill' || creep.carry.energy === 0) {
+    creep.moveTo(creep.room.storage);
+    creep.room.storage.transferEnergy(creep,creep.carryCapacity - creep.carry.energy);
+    lca(creep, 'getting ' + creep.carryCapacity - creep.carry.energy + ' from storage.');
+  } else {
+    creep.moveTo(creep.room.controller);
+    creep.upgradeController(creep.room.controller);
+    lca(creep, 'upgrading controller, ' + creep.carry.energy + ' energy until empty.');
+  }
+}
--- src/creep-upgrader.js
+++ src/creep-upgrader.js
@@ -1,24 +1,62 @@
 var UPGRADER = {
-  1: [ MOVE, WORK, CARRY, CARRY]
+  1: [MOVE, WORK, CARRY, CARRY],
+  2: [MOVE, MOVE, WORK, CARRY, CARRY],
+  3: [MOVE, MOVE, CARRY, CARRY, CARRY, CARRY, WORK, WORK],
+  4: [MOVE, MOVE, MOVE, MOVE, CARRY, CARRY, CARRY, CARRY, WORK, WORK],
+  5: [MOVE, MOVE, MOVE, MOVE, CARRY, CARRY, CARRY, CARRY, WORK, WORK, WORK, WORK],
+  6: [MOVE, MOVE, MOVE, MOVE, MOVE, MOVE, CARRY, CARRY, CARRY, CARRY, WORK, WORK, WORK, WORK]
 }
 
 function processUpgraders(creeps){
-  
+  var index = 0;
+
+  log('[Upgraders] -------------------','creep');
+
+  var sources = null;
+  var source = null;
+
+  for(var id in creeps) {
+    var creep = Game.getObjectById(creeps[id]);
+    index ++;
+
+    // lca(creep, creep.room.name + ' vs ' + p_room.name, true );
+    sources = creep.room.find(FIND_SOURCES);
+
+    if(sources.length > 1){
+      if(index % 2){
+        source = sources[0];
+      } else {
+        source = sources[1];
+      }
+    } else if(sources.length == 1) {
+      source = sources[0];
+    } else {
+        lca(creep, 'Odd - there are ' + sources.length ' sources in this room ' + creep.pos.roomName + ', and there is no code in creep-upgrader to deal with this.');
+        return OK;
+    }
+  }
+    upgrade(creep, source);
 }
 
-function upgrade(creep) {
+function upgrade(creep, source) {
   var spawn = creep.room.find(FIND_MY_SPAWNS)[0];
-  var sources = creep.room.find(FIND_SOURCES);
 
   switch(creep.memory.state){
   case 'fill':
     if(creep.carry.energy == creep.carryCapacity) {
       creep.memory.state = 'upgrade';
     } else {
-      lca(creep, 'is gathering energy.');
-      creep.moveTo(sources[0]);
-      creep.harvest(sources[0]);
-      pickupEnergy(creep);
+      if(creep.room.storage){
+        lca(creep, 'is moving to storage to get energy.');
+        creep.moveTo(creep.room.storage);
+        pickupEnergy(creep);
+        creep.room.storage.transferEnergy(creep);
+      } else {
+        lca(creep, 'is gathering energy from a source.');
+        creep.moveTo(sources);
+        creep.harvest(sources);
+        pickupEnergy(creep);
+      }
     }
     break;
   case 'upgrade':
